1da7a0c5377e67a1bd97b41088c3dbe6d11bd098
Аюпов Денис (Ayupov_DS) <Ayupov_DS@directum.ru>
2024-07-23 15:46:00 +0500

    Задача №4 сделана

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите натуральное число");
+
+try
+{
+	UInt32 a = UInt32.Parse(Console.ReadLine());
+	if (a % 2 == 1)
+		Console.WriteLine("YES");
+	else if (a >= 2 && a <= 5)
+		Console.WriteLine("NO");
+	else if (a >= 6 && a <= 20)
+		Console.WriteLine("YES");
+	else Console.WriteLine("NO");
+}
+catch
+{
+	Console.WriteLine("Введенные данные некорректны");
+}	
