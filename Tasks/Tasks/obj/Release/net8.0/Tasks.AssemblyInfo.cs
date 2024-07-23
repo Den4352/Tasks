b8e952383641732ee3589983a36d9e2e4d59f1d0
Аюпов Денис (Ayupov_DS) <Ayupov_DS@directum.ru>
2024-07-23 16:38:19 +0500

    Задача №5 сделана

--- Tasks/Tasks/obj/Release/net8.0/Tasks.AssemblyInfo.cs
+++ Tasks/Tasks/obj/Release/net8.0/Tasks.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasks")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da7a0c5377e67a1bd97b41088c3dbe6d11bd098")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasks")]
