35156acaca6f2d5000c1f7ca26fdca3329e72e44
Аюпов Денис (Ayupov_DS) <Ayupov_DS@directum.ru>
2024-07-23 16:40:50 +0500

    Задача №5

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int[] array = {3, 4, 5, 6, 98, 79, 233, 434, 423, 3, 44, 56};
+for (int i = 0; i < array.Length; i++)
+{
+	Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+int t;
+for (int i = 0; i < array.Length; i++)
+{
+	for (int j = 0; j < array.Length - 1 - i; j++)
+	{
+		if (array[j] > array[j+1])
+		{
+
+			t = array[j];
+			array[j] = array[j+1];
+			array[j + 1] = t;
+		}
+	}
+}
+Console.WriteLine("Вывод отсортированного массива");
+for (int i = 0; i < array.Length; i++)
+{
+	Console.Write(array[i] + " ");
+}
