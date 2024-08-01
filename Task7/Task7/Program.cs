a59828a8dbe85eff1dff9ddb9a4619c855ae8cf6
Аюпов Денис (Ayupov_DS) <Ayupov_DS@directum.ru>
2024-08-01 12:00:14 +0500

    Решена 7 задача

---
+++ Task7/Task7/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+try
+{
+	string s = Console.ReadLine(); //Введенное число в string
+	int num = Int32.Parse(s); //Введенное число в int
+	if (num > 0 && num <= Math.Pow(10, 9) && s[s.Length] != 0) //Условия работы программы (натуральное число, меньше 10^9, не заканчивается на 0)
+	{
+		char[] str = s.ToCharArray(); //массив элементов введеного числа
+		Array.Reverse(str); //реверс массива
+		int numRevers = Int32.Parse(str); //реверс числа
+		if (num == numRevers) //проверка числа с реверсом
+			Console.WriteLine("YES");
+		else
+			Console.WriteLine("NO");
+	}
+	else
+		Console.WriteLine("Ошибка ввода");
+}
+catch
+{
+	Console.WriteLine("Ошибка ввода");
+}
+
+
